845d425279659d1efcd8e4bcbcf19ed4000ddabe
jessicammelo <jessica.08041992@gmail.com>
2023-04-23 17:45:34 -0300

    ajustes

---
+++ trabalho-assinatura/src/assinatura/Application.java
@@ -0,0 +1,28 @@
+package assinatura;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("1 - gerar chaves");
+		System.out.println("2 - assinar");
+		System.out.println("3 - validar assinar recebida");
+		Scanner keyboard = new Scanner(System.in);
+		int operation = keyboard.nextInt();
+		keyboard.close();
+		switch (operation) {
+		case 1:
+			(new GenerationKeys()).Key();
+			break;
+		case 2:
+			(new Signature()).sign();
+			break;
+		case 3:
+		default:
+			(new ValidateSignature()).validate();
+			break;
+		}
+	}
+}
